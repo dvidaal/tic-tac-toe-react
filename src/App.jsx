6631bfa84dfcdb69dacc0abaf8924e58692f11d7
Diana Vidal <dvidaal@gmail.com>
2023-08-10 16:58:08 +0200

    Create functionality to reset the game

--- src/App.jsx
+++ src/App.jsx
@@ -21,11 +21,16 @@ const App = () => {
     setTurn(newTurn);
   };
 
+  const resetGame = () => {
+    setBoard(Array(9).fill(null));
+    setTurn(turns.x);
+  };
+
   return (
     <>
       <main className="board">
         <h1 className="main-title">Tic Tac Toe</h1>
-        <Button />
+        <Button resetGame={resetGame} />
         <section className="game">
           {board.map((cell, index) => (
             <Square key={index} index={index} updateBoard={updateBoard}>
